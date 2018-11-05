c63feb4ebc6eb5c4930372faf77ac6eae32341ea
Rohithmarktricks <rohithbanka05@msitprogram.net>
2018-11-05 23:19:08 +0530

    "ads2_m7_a1_1: 6/6 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 1/1 "

--- ADS---2 Assignments/m7/1. MST/Solution.java
+++ ADS---2 Assignments/m7/1. MST/Solution.java
@@ -3,34 +3,34 @@ import java.util.Scanner;
 /**
  * Class for solution.
  */
-public class Solution {
-	/**
-	 * Constructs the object.
-	 */
-	private Solution() {
-		//unused.
-	}
-	/**
-	 * Main function.
-	 *
-	 * @param      args  The arguments
-	 */
-	public static void main(final String[] args) {
-		Scanner scan = new Scanner(System.in);
-		int vertices = Integer.parseInt(scan.nextLine());
-		int edges = Integer.parseInt(scan.nextLine());
-		EdgeWeightedGraph edGraph = new EdgeWeightedGraph(vertices);
-		while (edges > 0) {
-			String[] tokens = scan.nextLine().split(" ");
-			int a = Integer.parseInt(tokens[0]);
-			int b = Integer.parseInt(tokens[1]);
-			double weight = Double.parseDouble(tokens[2]);
-			Edge edge = new Edge(a, b, weight);
-			edGraph.addEdge(edge);
-			edges--;
-		}
-		LazyPrimMST mst = new LazyPrimMST(edGraph);
-		System.out.printf("%.5f\n", mst.weight());
+public final class Solution {
+    /**
+     * Constructs the object.
+     */
+    private Solution() {
+        //unused.
+    }
+    /**
+     * Main function.
+     *
+     * @param      args  The arguments
+     */
+    public static void main(final String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int vertices = Integer.parseInt(scan.nextLine());
+        int edges = Integer.parseInt(scan.nextLine());
+        EdgeWeightedGraph edGraph = new EdgeWeightedGraph(vertices);
+        while (edges > 0) {
+            String[] tokens = scan.nextLine().split(" ");
+            int a = Integer.parseInt(tokens[0]);
+            int b = Integer.parseInt(tokens[1]);
+            double weight = Double.parseDouble(tokens[2]);
+            Edge edge = new Edge(a, b, weight);
+            edGraph.addEdge(edge);
+            edges--;
+        }
+        LazyPrimMST mst = new LazyPrimMST(edGraph);
+        System.out.printf("%.5f\n", mst.weight());
 
-	}
+    }
 }
