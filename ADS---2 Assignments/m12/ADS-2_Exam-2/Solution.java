4f9c41683bdde33b0bc807909689aaeb906cdfc1
Rohithmarktricks <rohithbanka05@msitprogram.net>
2018-11-10 16:04:58 +0530

    "ads2_m12_a1_1: 0/6 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

---
+++ ADS---2 Assignments/m12/ADS-2_Exam-2/Solution.java
@@ -0,0 +1,58 @@
+import java.util.Scanner;
+public class Solution {
+
+	public static void main(String[] args) {
+		// Self loops are not allowed...
+		// Parallel Edges are allowed...
+		// Take the Graph input here...
+		Scanner scan = new Scanner(System.in);
+		int cities = Integer.parseInt(scan.nextLine());
+		int routes = Integer.parseInt(scan.nextLine());
+		EdgeWeightedGraph eg = new EdgeWeightedGraph(cities);
+		Edge e;
+		while (cities > 0) {
+			String[] testcases = scan.nextLine().split(" ");
+			int source = Integer.parseInt(testcases[0]);
+			int destination = Integer.parseInt(testcases[1]);
+			int distance = Integer.parseInt(testcases[3]);
+			e = new Edge(source, destination, distance);
+			eg.addEdge(e);
+			cities--;
+		}
+		String caseToGo = scan.nextLine();
+		switch (caseToGo) {
+		case "Graph":
+			for (Edge ed : eg.edges()) {
+				System.out.println(ed);
+			}
+			break;
+
+		case "DirectedPaths":
+			// Handle the case of DirectedPaths, where two integers are given.
+			// First is the source and second is the destination.
+			// If the path exists print the distance between them.
+			// Other wise print "No Path Found."
+			String[] pair = scan.nextLine().split(" ");
+			int home = Integer.parseInt(pair[0]);
+			SP l = new SP(eg, home);
+			int destination = Integer.parseInt(pair[1]);
+			System.out.printf("%d\n", (int) l.distTo(destination));
+			
+			// System.out.println("No Path Found.");
+
+			break;
+
+		case "ViaPaths":
+			// Handle the case of ViaPaths, where three integers are given.
+			// First is the source and second is the via is the one where path should pass throuh.
+			// third is the destination.
+			// If the path exists print the distance between them.
+			// Other wise print "No Path Found."
+			break;
+
+		default:
+			break;
+		}
+
+	}
+}
