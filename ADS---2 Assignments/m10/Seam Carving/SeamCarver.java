82f0ae649e8be67347cacc1fbcfdf8bca4143d52
Rohithmarktricks <rohithbanka05@msitprogram.net>
2018-11-26 10:43:33 +0530

    "ads2_m10_a1_1: 9/9 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

--- ADS---2 Assignments/m10/Seam Carving/SeamCarver.java
+++ ADS---2 Assignments/m10/Seam Carving/SeamCarver.java
@@ -1,237 +1,292 @@
-import java.util.Arrays;
 import java.awt.Color;
 
+/**
+ * Class for seam carver.
+ */
 public class SeamCarver {
-	// create a seam carver object based on the given picture
-	private static final int BOUNDARY = 1000;
-	private Picture pic;
-	public SeamCarver(Picture picture) {
-		this.pic = new Picture(picture);
+	/**
+	 * varaiable declaration.
+	 */
+	private static final double BORDER = 1000;
+	/**
+	 * varaiable declaration.
+	 */
+	private Picture picture;
+	/**
+	 * Constructs the object.
+	 *
+	 * @param      p  The picture
+	 */
+	public SeamCarver(final Picture p) {
+		if (p == null) {
+			throw new java.lang.IllegalArgumentException("picture is null");
+		}
+		this.picture = new Picture(p);
 	}
-	// current picture
+
+
+	/**
+	 * picture method.
+	 *
+	 * @return picture.
+	 */
 	public Picture picture() {
-		return pic;
+		return this.picture;
 	}
-	// width of current picture
+	/**
+	 * // width of current picture.
+	 *
+	 * @return width.
+	 */
+
+
 	public int width() {
-		return pic.width();
+		return this.picture.width();
 	}
 
-	// height of current picture
+	/**
+	 * // height of current picture.
+	 *
+	 * @return height of current picture.
+	 */
 	public int height() {
-		return pic.height();
+		return this.picture.height();
 	}
 
-	// energy of pixel at column x and row y
-	public double energy(int x, int y) {
-		// if (x < 0 || x >= pic.width()
-		//         || y < 0 || y >= pic.height()) {
-		// 	throw new IndexOutOfBoundsException("Invalid position.");
-		// }
-
-//		it is located on the boundary
-		if (x == 0 || x == pic.width() - 1
-		        || y == 0 || y == pic.height() - 1) {
-			return SeamCarver.BOUNDARY;
+	/**
+	 * // energy of pixel at column x and row y.
+	 *
+	 * @param      x  integer.
+	 * @param      y  integer.
+	 *
+	 * @return  // energy of pixel at column x and row y.
+	 */
+	public  double energy(final int x, final int y) {
+		// int w = width() - 1, h = height() - 1;
+		if (x < 0 || x > width() - 1 || y < 0 || y > height() - 1) {
+			throw new java.lang.
+			IllegalArgumentException("IllegalArgumentException");
 		}
-
-//		calculate gradient on x axis
-		Color c1 = pic.get(x - 1, y);
-		Color c2 = pic.get(x + 1, y);
-		int rx = c2.getRed() - c1.getRed();
-		int gx = c2.getGreen() - c1.getGreen();
-		int bx = c2.getBlue() - c1.getBlue();
-		int dx = rx * rx + gx * gx + bx * bx;
-
-//		calculate gradient on y axis
-		c1 = pic.get(x, y - 1);
-		c2 = pic.get(x, y + 1);
-		int ry = c2.getRed() - c1.getRed();
-		int gy = c2.getGreen() - c1.getGreen();
-		int by = c2.getBlue() - c1.getBlue();
-		int dy = ry * ry + gy * gy + by * by;
-
-		return (dx + dy);
+		if (x == 0 || x == width() - 1 ||  y == 0 || y == height() - 1) {
+			return BORDER;
+		}
+		return internalEnergy(x, y);
 	}
 
-	// sequence of indices for horizontal seam
-	public int[] findHorizontalSeam() {
-		int h = pic.height();
-		int w = pic.width();
-		int total = h * w;
-		double[] distTo = new double[total + 2];
-		int[] edgeTo = new int[total + 2];
-		Arrays.fill(distTo, Double.MAX_VALUE);
-
-		//For source vertex.
-		distTo[0] = 0;
-		edgeTo[0] = -1;
-
-		for (int i = 0; i <= total; i++) {
-			//if i is source.
-			if (i == 0) {
-				for (int j = 0; j <= h; j++) {
-					int index = j + 1;
-					double cost = this.energy(0, j);
-					if (distTo[0] + cost < distTo[index]) {
-						distTo[index] = distTo[0] + cost;
-						edgeTo[index] = 0;
-					}
-				}
-				continue;
-			}
-
-			int col = (i - 1) / h;
-			int row = (i - 1) % h;
-//			current vertex is in last row
-			if (col == w - 1) {
-//				there is zero cost from current vertex to sink
-				double cost = 0;
-				if (distTo[i] + cost < distTo[total + 1]) {
-					distTo[total + 1] = distTo[i];
-					edgeTo[total + 1] = i;
-				}
-				continue;
-			}
-//			other cases
-			for (int j = -1; j <= 1; j++) {
-				int nrow = row + j;
-				int ncol = col + 1;
-				if (nrow < 0 || nrow >= h) continue;
-				int index = nrow + ncol * h + 1;
-				double cost = this.energy(ncol, nrow);
-				if (distTo[i] + cost < distTo[index]) {
-					distTo[index] = distTo[i] + cost;
-					edgeTo[index] = i;
-				}
-			}
-		} // end for loop
-
-		int[] result = new int[w];
-		int current = total + 1;
-		int ptr = w - 1;
-//		exclude source vertex, whose index is 0
-		while (edgeTo[current] > 0) {
-			result[ptr] = (edgeTo[current] - 1) % h;
-			ptr--;
-			current = edgeTo[current];
-//			should not happen
-			if (ptr < -1) {
-				System.err.println("Error occurs");
+	/**
+	 * // energy of pixel at column x and row y not on boarder.
+	 *
+	 * @param      x  integer
+	 * @param      y  integer
+	 *
+	 * @return  // energy of pixel at column x and row y not on boarder
+	 */
+	private double internalEnergy(final int x, final int y) {
+		Color left = this.picture.get(x - 1, y);
+		Color right = this.picture.get(x + 1, y);
+		Color up = this.picture.get(x, y - 1);
+		Color down = this.picture.get(x, y + 1);
+		return Math.sqrt(gradient(left, right) + gradient(up, down));
+	}
+	/**
+	 * gradient method.
+	 *
+	 * @param      first   One
+	 * @param      second   Two
+	 *
+	 * @return  gradient horizontal and vertical.
+	 */
+	private double gradient(final Color first, final Color second) {
+		double red = first.getRed() - second.getRed();
+		double green = first.getGreen() - second.getGreen();
+		double blue = first.getBlue() - second.getBlue();
+		return red * red + green * green + blue * blue;
+	}
+	/**
+	 * energy storage.
+	 *
+	 * @return  energies
+	 */
+	private double[][] initEnergies() {
+		double[][] energylist = new double[height()][width()];
+		for (int i = 0; i < height(); i++) {
+			for (int j = 0; j < width(); j++) {
+				energylist[i][j] = energy(j, i);
 			}
 		}
-		return result;
-	} // end method findHorizontalSeam
+		return energylist;
+	}
+	/**
+	 *  // pass through an array and mark the.
+	 *   shorthest distance from top to entry.
+	 *
+	 * @param      energies  The energies
+	 */
 
-	// sequence of indices for vertical seam
-	public int[] findVerticalSeam() {
-		int h = pic.height();
-		int w = pic.width();
-		int total = h * w;
-//		need to add one source and one sink
-//		to simplify the calculation of shortest path
-		double[] distTo = new double[total + 2];
-		int[] edgeTo = new int[total + 2];
-		Arrays.fill(distTo, Double.MAX_VALUE);
-
-//		set parameters for source vertex
-		distTo[0] = 0;
-		edgeTo[0] = -1;
-//		relax edges in topological order
-		for (int i = 0; i <= total; i++) {
-//			current vertex is source
-			if (i == 0) {
-				for (int j = 0; j < w; j++) {
-					double cost = this.energy(j, 0);
-					int index = j + 1;
-					if (distTo[0] + cost < distTo[index]) {
-						distTo[index] = distTo[0] + cost;
-						edgeTo[index] = 0;
-					}
-				} // end for loop
-				continue;
-			}
-			int row = (i - 1) / w;
-			int col = (i - 1) % w;
-//			current vertex is in last row
-			if (row == h - 1) {
-//				there is zero cost from current vertex to sink
-				double cost = 0;
-//				StdOut.println("Disto0: "+distTo[i]);
-				if (distTo[i] + cost < distTo[total + 1]) {
-					distTo[total + 1] = distTo[i];
-					edgeTo[total + 1] = i;
+	private void topologicalSort(final double[][] energies) {
+		int h = energies.length, w = energies[0].length;
+		for (int row = 1; row < h; row++) {
+			for (int col = 0; col < w; col++) {
+				double temp = energies[row - 1][col];
+				double min = 0;
+				if (col == 0) {
+					min = temp;
+				} else {
+					min = Math.min(temp, energies[row - 1][col - 1]);
 				}
-				continue;
-			}
-//			other cases
-			for (int j = -1; j <= 1; j++) {
-				int nrow = row + 1;
-				int ncol = col + j;
-				if (ncol < 0 || ncol >= w) continue;
-				int index = nrow * w + ncol + 1;
-				double cost = this.energy(ncol, nrow);
-				if (distTo[i] + cost < distTo[index]) {
-					distTo[index] = distTo[i] + cost;
-					edgeTo[index] = i;
-//					StdOut.println("Updated: "+distTo[index]);
-				}
-			}
-		} // end for loop
 
-		int[] result = new int[h];
-		int current = total + 1;
-		int ptr = h - 1;
-//		exclude source vertex, whose index is 0
-		while (edgeTo[current] > 0) {
-			result[ptr] = (edgeTo[current] - 1) % w;
-			ptr--;
-			current = edgeTo[current];
-//			should not happen
-			if (ptr < -1) {
-				System.err.println("Error occurs");
+				if (col != (w - 1)) {
+					min = Math.min(min, energies[row - 1][col + 1]);
+				} else {
+					min = min;
+				}
+				energies[row][col] += min;
 			}
 		}
-		return result;
-	} // end method findVerticalSeam
 
-	// remove horizontal seam from current picture
-	public void removeHorizontalSeam(int[] seam) {
-		int w = pic.width();
-		int h = pic.height();
-		Picture newPic = new Picture(w, h - 1);
+	}
+	/**
+	 * transpose matrix.
+	 *
+	 * @param      energies  The energies
+	 *
+	 * @return   transposed energies.
+	 */
+	private double[][] transposeGrid(final double[][] energies) {
+		int h = energies.length, w = energies[0].length;
+		double[][] transEnergies = new double[w][h];
+		for (int row = 0; row < h; row++) {
+			for (int col = 0; col < w; col++) {
+				transEnergies[col][row] = energies[row][col];
+			}
+		}
+		return transEnergies;
+	}
+	/**
+	 * vertical path.
+	 *
+	 * @param      energies  The energies
+	 *
+	 * @return vertical energies.
+	 */
+	private int[] minVerticalPath(final double[][] energies) {
+		int h = energies.length, w = energies[0].length;
+		int[] path = new int[h];
+
+		topologicalSort(energies);
+
+		// find the lowest element in last row
+		path[h - 1] = 0;
 		for (int i = 0; i < w; i++) {
-			for (int j = 0; j < h - 1; j++) {
-				if (j < seam[i]) {
-					newPic.set(i, j, pic.get(i, j));
-				} else {
-					h--;
-					newPic.set(i, j, pic.get(i, j + 1));
-				}
+			if (energies[h - 1][i] < energies[h - 1][path[h - 1]]) {
+				path[h - 1] = i;
 			}
 		}
-		this.pic = newPic;
+		// trace path back to first row
+		// assuming we need the cheapest upper neighboring entry
+		for (int row = h - 2; row >= 0; row--) {
+			int col = path[row + 1];
+			// three neighboring, priority to center
+			path[row] = col;
+			if (col > 0 && energies[row][col - 1] < energies[row][path[row]]) {
+				path[row] = col - 1;
+			}
+			if (col < (w - 2) && energies[row][col + 1]
+			        < energies[row][path[row]]) {
+				path[row] = col + 1;
+			}
+		}
+		return path;
+	}
+	/**
+	 * // sequence of indices for horizontal seam.
+	 *
+	 * @return  // sequence of indices for horizontal seam.
+	 */
+	public int[] findHorizontalSeam() {
+		double[][] transposeEnergies = transposeGrid(initEnergies());
+		return minVerticalPath(transposeEnergies);
+	}
+	/**
+	 *  // sequence of indices for vertical seam.
+	 *
+	 * @return   // sequence of indices for vertical seam.
+	 */
+	public int[] findVerticalSeam() {
+		double[][] normalEnergies = initEnergies();
+		return minVerticalPath(normalEnergies);
+	}
+	/**
+	 * Removes a horizontal seam.
+	 *
+	 * @param      a int array.
+	 */
+	public void removeHorizontalSeam(final int[] a) {
+		if (height() <= 1 || !isValid(a, width(), height() - 1)) {
+			throw new
+			java.lang.IllegalArgumentException("IllegalArgumentException");
+		}
+		Picture pic = new Picture(width(), height() - 1);
+		for (int w = 0; w < width(); w++) {
+			for (int h = 0; h < a[w]; h++) {
+				pic.set(w, h, this.picture.get(w, h));
+			}
 
-		return;
+			for (int h = a[w] + 1; h < height(); h++) {
+				pic.set(w, h - 1, this.picture.get(w, h));
+			}
+
+		}
+		this.picture = pic;
 	}
 
+	/**
+	 * Removes a vertical seam.
+	 *
+	 * @param      a  int array.
+	 */
+	public void removeVerticalSeam(final int[] a) {
+		if (width() <= 1 || !isValid(a, height(), width())) {
+			throw new
+			java.lang.IllegalArgumentException("IllegalArgumentException");
+		}
+		Picture pic = new Picture(width() - 1, height());
+		for (int h = 0; h < height(); h++) {
+			for (int w = 0; w < a[h]; w++) {
+				pic.set(w, h, this.picture.get(w, h));
+			}
 
-	// remove vertical seam from current picture
-	public void removeVerticalSeam(int[] seam) {
-		int w = pic.width();
-		int h = pic.height();
-		Picture newPic = new Picture(w, h - 1);
-		for (int j = 0; j < h; j++) {
-			for (int i = 0; i < w - 1; i++) {
-				if (i < seam[j]) {
-					newPic.set(i, j, pic.get(i, j));
-				} else {
-					w--;
-					newPic.set(i, j, pic.get(i + 1, j));
-				}
+
+			for (int w = a[h] + 1; w < width(); w++) {
+				pic.set(w - 1, h, this.picture.get(w, h));
+			}
+
+		}
+		this.picture = pic;
+	}
+
+	/**
+	 * // return false if two consecutive entries differ by more than 1.
+	 *
+	 * @param      a      { parameter_description }
+	 * @param      len    The length
+	 * @param      range  The range
+	 *
+	 * @return     True if valid, False otherwise.
+	 */
+	private boolean isValid(final int[] a, final int len, final int range) {
+		if (a == null) {
+			return false;
+		}
+		if (a.length != len || a[0] < 0 || a[0] > range) {
+			return false;
+		}
+		for (int i = 1; i < len; i++) {
+			if (a[i] < Math.max(0, a[i - 1] - 1)
+			        || a[i] > Math.min(range, a[i - 1] + 1)) {
+				return false;
 			}
 		}
-		this.pic = newPic;
-		return;
+		return true;
 	}
 }
